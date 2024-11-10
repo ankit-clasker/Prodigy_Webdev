3b33211a94555e931d9c508fed6b4200657f7356
Ankit Kumar Yadav <ankit2005pky@gmail.com>
2024-11-10 18:44:53 +0530

    Add files via upload

---
+++ Prodigy/Prodigy_WD_01/script.js
@@ -0,0 +1,11 @@
+// Select the navigation bar
+const navbar = document.getElementById('navbar');
+
+// Add scroll event listener
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
